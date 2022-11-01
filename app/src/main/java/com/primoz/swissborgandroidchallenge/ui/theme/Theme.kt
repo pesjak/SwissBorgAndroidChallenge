4e98422954ee351209bf2958e9b0f237d6f919ce
Primož Pesjak <primoz.pesjak@gmail.com>
2022-11-01 17:05:57 +0100

    added ktlint

--- app/src/main/java/com/primoz/swissborgandroidchallenge/ui/theme/Theme.kt
+++ app/src/main/java/com/primoz/swissborgandroidchallenge/ui/theme/Theme.kt
@@ -28,7 +28,10 @@ private val LightColorPalette = lightColors(
 )
 
 @Composable
-fun SwissBorgAndroidChallengeTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
+fun SwissBorgAndroidChallengeTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit
+) {
     val colors = if (darkTheme) {
         DarkColorPalette
     } else {
